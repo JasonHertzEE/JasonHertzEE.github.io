c446237c63cb0bf1315ef925fb93d1a5d9b9daf9
JasonHertzEE <jason.hertz.ee@gmail.com>
2026-07-25 22:53:47 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,7 +102,7 @@
 		// Toggle.
 			$(
 				'<div id="headerToggle">' +
-					'<a href="#header" class="toggle">MENU</a>' +
+					'<a href="#header" class="toggle"></a>' +
 				'</div>'
 			)
 				.appendTo($body);
